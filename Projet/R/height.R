ceda6f989b30000827fef6cb3c9271f69c50f31d
Julien Nique <nique.julien@neuf.fr>
2020-10-22 18:50:41 +0200

    MAJ

--- Projet/R/height.R
+++ Projet/R/height.R
@@ -1,7 +1,15 @@
 height <- function(h, hmax, v, ftn){
+  if (volume(h, hmax, ftn) + v <= 0){
+    return(0)
+  }
+  else if (volume(h, hmax, ftn) + v > volume(hmax, hmax, ftn)){
+    return(hmax)
+  }
+  else{
   u <- Newton1d(h, function(u){
     volume(u, hmax, ftn) - volume(h, hmax, ftn) - v}, 1e-6)
   return(u)
+  }
 }
 
 height <- Vectorize(height, vectorize.args = c("h","v"))
